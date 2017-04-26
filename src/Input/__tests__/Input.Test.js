d5e8d0c874f53772738a1ece225183d887162e1b
Parth Mehta <parth@bluechilli.com>
2017-04-26 16:05:44 +1000

    Input Tests

---
+++ src/Input/__tests__/Input.Test.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {isEqual} from 'lodash';
+import {Input} from '../Input.tsx';
+
+// Defining all the properties
+const allInputProps = {
+    autoFocus: false,
+    min: 9,
+    max: 999,
+    minLength: 4,
+    maxLength: 10,
+    pattern: '^[9][0-9]?$|^999$',
+    labelPrefix: 'LabelPrefix',
+    labelPostfix: 'LabelPostfix',
+    label: 'InputLabel',
+    type: 'email',
+    prepend: '$',
+    append: '%',
+    name: 'SampleInput',
+    className: 'SampleInputClass',
+    id: 'InputId',
+    onChange: () => {},
+    onBlur: () => {}
+};
+
+// Destructing the props so as to check them individually
+const {autoFocus, min, max, minLength, maxLength, pattern,
+       labelPrefix, labelPostfix, label, type, prepend,
+       append, name, className, id, onChange, onBlur} = allInputProps;
+
+const inputWrapperProps = {
+    className: 'SampleInputClass input',
+    type,
+    name,
+    labelPostfix,
+    labelPrefix,
+    label
+};
+
+const inputGroupProps = {
+    prepend,
+    append
+};
+
+const inputBaseProps = {
+    autoFocus,
+    onChange,
+    onBlur,
+    id,
+    minLength,
+    maxLength,
+    min,
+    max,
+    type,
+    name,
+    pattern
+};
+
+const displayValidationProps = {
+    minLength,
+    maxLength,
+    min,
+    max,
+    type,
+    name,
+    pattern
+};
+
+describe('<Input />', () => {
+    const wrapper = shallow(<Input {...allInputProps} />);
+    
+    const {children:InputWrapperChildren, ...InputWrapperProps} = wrapper.first().props();
+    const {children:InputGroupChildren, ...InputGroupProps} = InputWrapperChildren[0].props;
+    const {children:InputBaseChildren, ...InputBaseProps} = InputGroupChildren.props;
+    const {children:DisplayValidationChildren, ...DisplayValidationProps} = InputWrapperChildren[1].props;
+    
+    // InputWrapper
+    it('should only have the required props for <InputWrapper />', () => {
+        expect(isEqual(InputWrapperProps, inputWrapperProps)).toBe(true);
+    });
+
+    // InputGroup
+    it('should only have the required props for <InputGroup />', () => {
+        expect(isEqual(InputGroupProps, inputGroupProps)).toBe(true);
+    });
+
+    // InputBase
+    it('should only have the required props for <InputBase />', () => {
+        expect(isEqual(InputBaseProps, inputBaseProps)).toBe(true);
+    });
+
+    // DisplayValidation
+    it('should only have the required props for <DisplayValidation />', () => {
+        expect(isEqual(DisplayValidationProps, displayValidationProps)).toBe(true);
+    });
+});
