f600d55f23c12a0914dd74f72c6784de14a9701a
shane <shane@shanes-MacBook-Pro.local>
2017-04-27 17:09:58 +1000

    Exports setup

--- src/Form/FormReduxWrapper.tsx
+++ src/Form/FormReduxWrapper.tsx
@@ -1,24 +1,26 @@
 import React from "react";
 import {Map} from "immutable";
-import Form, {FormProps} from "./Form";
+import Form, {FormProps, FormOptionalProps} from "./Form";
 import {connect, Dispatch} from "react-redux";
 
 interface MapStateToProps {
   FormState: Map<string, any>
 }
+
 interface MapDispatchToProps {
   dispatch: Dispatch<any>
 }
 
 
-const FormReduxWrapper = (props:FormProps<undefined> & MapStateToProps & MapDispatchToProps) =>(
-  <Form {...props}/>
-);
+const FormReduxWrapper = (props:FormProps<undefined> & MapStateToProps & MapDispatchToProps) => <Form {...props}/>
 
 const mapStateToProps = (state:Map<string, any>):MapStateToProps => {
   return {
     FormState: state.get('FormState', Map()) || Map()
   }
 }
-export {FormProps}
+
+
+
+export {FormProps, FormOptionalProps}
 export default connect<MapStateToProps, MapDispatchToProps, FormProps<undefined>>(mapStateToProps)(FormReduxWrapper);
