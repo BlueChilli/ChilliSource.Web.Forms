8815c94b2ea3785ec494ee77a8a7223372dde86c
Parth Mehta <parth@bluechilli.com>
2017-04-27 11:15:04 +1000

    fixes #8

--- src/Form/Helpers/__tests__/performanceWrapper.tests.ts
+++ src/Form/Helpers/__tests__/performanceWrapper.tests.ts
@@ -40,20 +40,28 @@ const inputPathPlainProps = {
 }
 
 
-describe('perfomanceWraper', () => {
+describe('performanceWrapper', () => {
+
+  // get HTMl attributes
   describe('getHTMLAttributes', () => {
-    it('returns all supported attributes', () => {
+
+    // supported attributes
+    it('should return all supported attributes', () => {
       const returnedSupportedProps = getHTMLAttributes()(supportedProps);
       expect(isEqual(supportedProps, returnedSupportedProps)).toBe(true);
     })
-    it('excludes unsupported attributes', () => {
+
+    // unsupported attributes
+    it('should exclude unsupported attributes', () => {
       const returnedSupportedProps = getHTMLAttributes()(Object.assign({}, supportedProps, unsupportedProps))
       expect(isEqual(supportedProps, returnedSupportedProps)).toBe(true);
     })
   });
 
+  // input path
   describe('getInputPath', () => {
-    it('always returns an array', () => {
+
+    it('should always return inputPath as an array', () => {
       const inputPathFieldset = getInputPath(inputPathFieldsetProps)();
       const inputPathArray = getInputPath(inputPathInputArrayProps)();
       const inputPathPlain = getInputPath(inputPathPlainProps)();
@@ -62,42 +70,52 @@ describe('perfomanceWraper', () => {
       expect(isArray(inputPathArray)).toBe(true);
       expect(isArray(inputPathPlain)).toBe(true);
     });
-    it('namespaces fieldsets', () => {
+
+    it('should include fieldset name in the inputPath', () => {
       const inputPath = getInputPath(inputPathFieldsetProps)();
       expect(isEqual(inputPath, ['fieldset', 'name'])).toBe(true);
     });
-    it('namespaces input arrays', () => {
+
+    it('should includes array-formatted name(i.e. _name_[]) in the inputPath', () => {
       const inputPath = getInputPath(inputPathInputArrayProps)();
       expect(isEqual(inputPath, ['name[]', 'id'])).toBe(true);
     });
-    it('does not namespace plain inputs', () => {
+
+    it('should not namespace plain inputs', () => {
       const inputPath = getInputPath(inputPathPlainProps)();
       expect(isEqual(inputPath, ['name'])).toBe(true);
     });
   });
 
   describe('getPrioritisedDefaultValue', () => {
+    
     it('should prioritise defaultValue', () => {
       expect(getPrioritisedDefaultValue('defaultValue', 'defaultChecked', 'defaultSelected')).toBe('defaultValue');
     });
+    
     it('should prioritise defaultChecked', () => {
       expect(getPrioritisedDefaultValue(undefined, 'defaultChecked', 'defaultSelected')).toBe('defaultChecked');
     });
+    
     it('should prioritise defaultSelected', () => {
       expect(getPrioritisedDefaultValue(undefined, undefined, 'defaultSelected')).toBe('defaultSelected');
     });
   });
 
   describe('getPrioritisedValue', () => {
+    
     it('should prioritise value', () => {
       expect(getPrioritisedValue('value', 'inputInfoValue', 'prioritisedDefaultValue', false)).toBe('value');
     });
+    
     it('should prioritise inputInfoValue', () => {
       expect(getPrioritisedValue(undefined, 'inputInfoValue', 'prioritisedDefaultValue', false)).toBe('inputInfoValue');
     });
+    
     it('should prioritise prioritisedDefaultValue', () => {
       expect(getPrioritisedValue(undefined, undefined, 'prioritisedDefaultValue', false)).toBe('prioritisedDefaultValue');
     });
+    
     it('should prioritise unsetValue', () => {
       expect(getPrioritisedValue(undefined, undefined, undefined, false)).toBe(false);
     });
