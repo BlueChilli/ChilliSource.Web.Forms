cc5b1092817274ed5ac4dd80e455165903c895dd
Parth Mehta <parth@bluechilli.com>
2017-04-28 10:31:04 +1000

    tests for RadioTab

---
+++ src/Radio/__tests__/RadioTab.Test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {isEqual} from 'lodash';
+import RadioTab from '../RadioTab';
+
+const allRadioTabProps = {
+    className: 'RadioTabClass radio-tab',
+    type: 'radio',
+    autoFocus: true,
+    onChange: event => {},
+    onBlur: event => {},
+    id: 'RadioTabId',
+    defaultChecked: true,
+    defaultSelected: true,
+    label: 'RadioTab Label',
+    name: 'RadioTabName',
+};
+
+const {className, type, ...remainingRadioTabProps} = allRadioTabProps;
+
+const removeChildrenProp = props => {
+    const {children, ...propsWithoutChildren} = props;
+    return propsWithoutChildren;
+}
+
+describe('<RadioTab />', () => {
+    it('should have the required props for <Switch />', () => {
+        const wrapper = shallow(<RadioTab className='RadioTabClass' {...remainingRadioTabProps} />);
+        const wrapperProps = removeChildrenProp(wrapper.props());
+        expect(isEqual(wrapperProps, allRadioTabProps)).toBe(true);
+    });
+});
