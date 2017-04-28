a5cb0d67eda6d833f4701dc1c05460f79c51c678
Parth Mehta <parth@bluechilli.com>
2017-04-29 07:42:32 +1000

    refactor

--- src/Select/MultiSelect.jsx
+++ src/Select/MultiSelect.jsx
@@ -1,43 +1,42 @@
-import React from "react"
-import Select from "react-select"
-import {List, fromJS} from "immutable";
-import "react-select/dist/react-select.css"
-import InputWrapper from "../../../Frecl/Components/Form/InputWrapper.tsx";
-import performanceWrapper from "../../../Frecl/Components/Form/Helpers/performanceWrapper.ts";
-import classnames from "classnames";
+import React from 'react';
+import Select from 'react-select';
+import {List, fromJS} from 'immutable';
+import 'react-select/dist/react-select.css';
+import InputWrapper from '../Form/InputWrapper';
+import {SelectInputProps} from "../Form/Types/types";
+import performanceWrapper from '../Form/Helpers/performanceWrapper';
+import classnames from 'classnames';
 
+export class MultiSelect extends React.PureComponent {
+  defaultProps = {
+    options: List([])
+  }
 
-const OptionSelect = React.createClass({
-  getDefaultProps(){
-    return {
-      options: List([])
+  componentWillMount() {
+    const {inputChanged, defaultValue} = this.props;
+    inputChanged(defaultValue || List([]), false)
+  }
+
+  handleChange = value => {
+    const {inputChanged, onChange} = this.props;
+
+    inputChanged(List([]).concat(fromJS(values)));
+    if(typeof onChange === 'function') {
+      onChange(values);
     }
-  },
-  componentWillMount(){
-    this.props.inputChanged(this.props.defaultValue || List([]), false)
-  },
+  }
 
-  handleChange(values){
-    this.props.inputChanged(List([]).concat(fromJS(values)))
-    if (this.props.onChange) {
-      this.props.onChange(values);
-    }
-  },
-
-  render()
-  {
-    const {options, getAttributes, value} = this.props;
+  render() {
+    const {options, value, className, label, labelPrefix, labelPostfix, ...props} = this.props;
     const safeValue = value !== '' ? List(value) : List();
-    const classes = classnames(this.props.className, 'input');
-
+    const classes = classnames(className, 'input');
 
     return (
-      <InputWrapper {...this.props} className={classes}>
+      <InputWrapper name={props.name} label={label} labelPrefix={labelPrefix} labelPostfix={labelPostfix} className={classes}>
         <Select value={safeValue.toJS()} options={options.toJS()} multi={true} onChange={this.handleChange}/>
-        {/*<Validate required={this.props.required} name={this.props.name}/>*/}
       </InputWrapper>
     )
   }
-});
+}
 
-export default performanceWrapper(OptionSelect);
+export default performanceWrapper(MultiSelect);
