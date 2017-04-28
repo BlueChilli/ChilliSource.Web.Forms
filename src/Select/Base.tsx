4f9c159d796f503e932ab872453c5bbb2c149b84
Parth Mehta <parth@bluechilli.com>
2017-04-28 15:28:03 +1000

    slight refactor

--- src/Select/Base.tsx
+++ src/Select/Base.tsx
@@ -6,34 +6,18 @@ import {getHTMLAttributes} from "../Form/Helpers/inputHelpers";
 import {SelectInputProps} from "../Form/Types/types";
 import {PerformanceWrapperProps} from "../Form/Helpers/performanceWrapper";
 
-interface OptionTypes{
-  value: string | boolean | number,
-  children: List<React.ReactText>
-}
-
-interface WithProps extends SelectInputProps {
-  defaultSelected?: string | boolean | number
-}
-
-
-const getDefaultSelected = ({children, defaultValue}:SelectInputProps) => {
-  if (Children.count(children) < 1) {
-    return '';
-  } else if (defaultValue) {
-    return defaultValue;
-  } else {
-    return (Children.toArray(children)[0] as React.ReactElement<OptionTypes>).props.value;
-  }
-};
-
 class SelectBase extends React.Component<SelectInputProps & PerformanceWrapperProps, {}> {
   displayName: 'SelectBase'
-  handleChange = (e) => {
-    this.props.inputChanged(e.target.value);
-    if (this.props.onChange) {
-      this.props.onChange(e);
+  
+  handleChange = event => {
+    const {inputChanged, onChange} = this.props;
+
+    inputChanged(event.target.value);
+    if(typeof onChange === 'function') {
+      onChange(event);
     }
   }
+
   render () {
     const attributes = getHTMLAttributes(this.props);
     return (
@@ -42,10 +26,6 @@ class SelectBase extends React.Component<SelectInputProps & PerformanceWrapperPr
       </select>
     );
   }
-};
+}
 
-export default withProps<WithProps, SelectInputProps & PerformanceWrapperProps>(props => {
-  return {
-    defaultSelected: getDefaultSelected(props)
-  }
-})(SelectBase);
+export default SelectBase;
