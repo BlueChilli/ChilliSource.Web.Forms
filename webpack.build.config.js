ec59442fca91513a4758c44a209d28d61461c340
shane <spp935@gmail.com>
2017-05-10 16:43:01 +1000

    build improvments

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -10,11 +10,12 @@ var path = require('path');
 process.env.NODE_ENV = JSON.stringify(config.get('buildEnvironment'));
 
 module.exports = {
-  entry: path.join(__dirname, '/app/entry'),
+  entry: path.join(__dirname, '/app/index'),
   output: {
-    path: __dirname,
+    path: path.join(__dirname, "/dist/"),
     libraryTarget: "umd",
-    "filename": "index.js"
+    library: "ChillisourceWebForms",
+    "filename": "chillisource-web-forms.js"
   },
   
   module: {
@@ -37,6 +38,64 @@ module.exports = {
       use: "raw-loader"
     }]
   },
+
+  externals: {
+    "classnames": {
+      commonjs: "classnames",
+      commonjs2: "classnames",
+      amd: "classnames",
+      root: "classnames"
+    },
+    "immutable": {
+      commonjs: "immutable",
+      commonjs2: "immutable",
+      amd: "immutable",
+      root: "Immutable"
+    },
+    "moment": {
+      commonjs: "moment",
+      commonjs2: "moment",
+      amd: "moment",
+      root: "Moment"
+    },
+    "react": {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      root: "React"
+    },
+    "react-date-range": {
+      commonjs: "react-date-range",
+      commonjs2: "react-date-range",
+      amd: "react-date-range",
+      root: "ReactDateRange"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "react-dom",
+      root: "ReactDOM"
+    },
+    "react-dropzone": {
+      commonjs: "react-dropzone",
+      commonjs2: "react-dropzone",
+      amd: "react-dropzone",
+      root: "ReactDropzone"
+    },
+    "react-onclickoutside": {
+      commonjs: "react-onclickoutside",
+      commonjs2: "react-onclickoutside",
+      amd: "react-onclickoutside",
+      root: "ReactOnclickoutside"
+    },
+    "recompose": {
+      commonjs: "recompose",
+      commonjs2: "recompose",
+      amd: "recompose",
+      root: "Recompose"
+    }
+  },
+
   plugins: [
     // new webpack.NoErrorsPlugin(),
     new webpack.LoaderOptionsPlugin({
