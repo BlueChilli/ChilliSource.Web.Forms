4a334a505d9367e1c8037bbc5bffda5ad9d649ff
shane <spp935@gmail.com>
2017-06-02 11:15:28 +1000

    Build improvements

--- typings/typescript.d.ts
+++ typings/typescript.d.ts
@@ -1,7 +1,5 @@
-import {CheckBox, DatePicker, DateRange, DropZone, Input, Number, Radio, RadioTab, RadioTabs, Select, TextArea, Validate, Validation, Form, FormProps, FormOptionalProps, ReduxReducer, Fieldset, types} from "../app/index"
+import {CheckBox, DatePicker, DateRange, DropZone, Input, Number, Radio, RadioTab, RadioTabs, Select, TextArea, Validate, Validation, Form, FormProps, FormOptionalProps, ReduxReducer, Fieldset} from "../app/index"
 
-declare module "chillisource.web.forms" {
-  export {
-    CheckBox, DatePicker, DateRange, DropZone, Input, Number, Radio, RadioTab, RadioTabs, Select, TextArea, Validate, Validation, Form, FormProps, FormOptionalProps, ReduxReducer, Fieldset, types
-  }
+export {
+  CheckBox, DatePicker, DateRange, DropZone, Input, Number, Radio, RadioTab, RadioTabs, Select, TextArea, Validate, Validation, Form, FormProps, FormOptionalProps, Fieldset
 }
