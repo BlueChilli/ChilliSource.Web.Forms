5972f52041e45dc7246fd6d3091b978d3ee52ea7
shane <spp935@gmail.com>
2017-06-06 11:47:01 +1000

    Version updates

--- typings/typescript.d.ts
+++ typings/typescript.d.ts
@@ -1,6 +1,6 @@
-
 // <reference types="react" />
 // <reference types="recompose" />
+
 import {CheckBox, DatePicker, DateRange, DropZone, Input, Number, Radio, RadioTab, RadioTabs, Select, TextArea, Validate, Validation, Form, FormProps, FormOptionalProps, ReduxReducer, Fieldset} from "../app/index"
 
 declare module "chillisource.web.forms" {
