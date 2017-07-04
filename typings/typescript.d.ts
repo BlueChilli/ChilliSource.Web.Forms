0c770b2f516b5759bedac973a3cda983c0a58162
shane <spp935@gmail.com>
2017-07-04 13:10:51 +1000

    Better typings file

--- typings/typescript.d.ts
+++ typings/typescript.d.ts
@@ -1,6 +1,15 @@
-import {CheckBox, DatePicker, DateRange, DropZone, Input, Number, Radio, RadioTab, RadioTabs, Select, MultiSelect, TextArea, Validate, Validation, Form, FormOwnProps, FormOptionalProps, ReduxReducer, Fieldset, types} from "../app/index"
+import {CheckBox as CheckBoxClass, DatePicker, DateRange, DropZone, Input, Number, Radio, RadioTab, RadioTabs, Select, MultiSelect, TextArea, Validate, Validation, Form, FormOwnProps, FormOptionalProps, ReduxReducer, Fieldset, types, performanceWrapper} from "../app/index"
+
+type CheckBox = CheckBoxClass;
+
+
+declare module "cs.forms"{
+  export {
+    CheckBox
+  }
+}
 
 export {
-  CheckBox, DatePicker, DateRange, DropZone, Input, Number, Radio, RadioTab, RadioTabs, Select, MultiSelect, TextArea, Validate, Validation, Form, FormOwnProps, FormOptionalProps, Fieldset, types
+  CheckBox, DatePicker, DateRange, DropZone, Input, Number, Radio, RadioTab, RadioTabs, Select, MultiSelect, TextArea, Validate, Validation, Form, FormOwnProps, FormOptionalProps, Fieldset, types, performanceWrapper
 }
 
