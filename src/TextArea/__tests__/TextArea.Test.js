1428d55f13b71bf41ecf0257e7d3e8a0ae8bcc47
Mitch Malone <mitch.malone@me.com>
2017-07-25 16:38:43 +1000

    TextArea tests WIP

--- src/TextArea/__tests__/TextArea.Test.js
+++ src/TextArea/__tests__/TextArea.Test.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import sinon from "sinon";
+import PropTypes from "prop-types";
+import {shallow, mount} from 'enzyme';
 import {isEqual} from 'lodash';
 import {TextArea} from '../TextArea';
+import Form from '../../Form/Form';
+import {Map} from "immutable";
 
 const allTextAreaProps = {
     autoFocus: false,
@@ -18,9 +22,7 @@ const allTextAreaProps = {
     id: 'TextAreaId'
 };
 
-const {autoFocus, min, max, minLength, maxLength, pattern,
-       labelPrefix, labelPostfix, label, prepend,
-       append, name, className, id} = allTextAreaProps;
+const {autoFocus, min, max, minLength, maxLength, pattern, labelPrefix, labelPostfix, label, prepend, append, name, className, id} = allTextAreaProps;
 
 const inputWrapperProps = {
     className: 'SampleTextAreaClass textarea input',
@@ -57,7 +59,7 @@ const removeChildrenProp = props => {
     return propsWithoutChildren;
 }
 
-describe('<TextArea />', () => {
+describe('<TextArea /> shallow', () => {
     const wrapper = shallow(<TextArea {...allTextAreaProps} />);
     const InputWrapperProps = removeChildrenProp(wrapper.find('InputWrapper').props());
     const InputGroupProps = removeChildrenProp(wrapper.find('InputGroup').props());
@@ -82,5 +84,51 @@ describe('<TextArea />', () => {
     // DisplayValidation
     it('should only have the required props for <DisplayValidation />', () => {
         expect(isEqual(DisplayValidationProps, displayValidationProps)).toBe(true);
+    });
+});
+
+describe('<TextArea /> mount', () => {
+    const defaultValue = "Hello, this is a Textarea";
+    const updatedDefaultValue = "Changed Input";
+    const onChangeCallback = sinon.spy();
+    const onBlurCallback = sinon.spy();
+
+    const wrapper = mount(
+        <Form name="textAreaTest">
+            <TextArea 
+                autoFocus={false}
+                labelPrefix="Label Prefix"
+                labelPostfix="Label Postfix"
+                label="TextArea Label"
+                name="SampleTextArea"
+                className="SampleTextAreaClass"
+                id="TextAreaId"
+                onBlur={onBlurCallback}
+                onChange={onChangeCallback}
+            />
+        </Form>
+    );
+
+    const label = wrapper.find('label');
+    const input = wrapper.find("textarea");
+
+    it('should set the className', () => {
+        expect(wrapper.find('.SampleTextAreaClass').exists()).toBe(true);
+    });
+
+    it('should set the label', () => {
+        expect(label.prop('children') === "TextArea Label").toBe(true);
+    });
+
+    it('should set the labelPrefix', () => {
+        expect(wrapper.find(".input-label-prefix").prop('children') === "Label Prefix").toBe(true);
+    });
+
+    it('should set the labelPostfix', () => {
+        expect(wrapper.find(".input-label-postfix").prop('children') === "Label Postfix").toBe(true);
+    });
+
+    it('should set labelFor and name to be the same', () => {
+        expect(label.prop("htmlFor") === input.prop("name")).toBe(true);
     });
 });
