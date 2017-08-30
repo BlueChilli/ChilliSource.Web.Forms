4d2c520bdb4b458d8952a2fe1d239194823a9105
Parth Mehta <parth@bluechilli.com>
2017-08-30 14:08:20 +1000

    :art: better dropzone

--- src/DropZone/DropZone.tsx
+++ src/DropZone/DropZone.tsx
@@ -1,112 +1,97 @@
 /** Libraries */
-import React from 'react';
+import React, {Children} from 'react';
 import {Set} from 'immutable';
-import ReactDropzone from 'react-dropzone';
+import DropZone from 'react-dropzone';
 import classnames from 'classnames';
 import {compose} from 'recompose';
 import {isFunction} from 'lodash';
 
-/** Components */
+/** * Components */
 import performanceWrapper from '../Form/Helpers/performanceWrapper';
 import {getHTMLAttributes} from '../Form/Helpers/inputHelpers';
-<<<<<<< HEAD:src/DropZone/DropZone.tsx
-import {DropZoneProps} from '../Form/Types/types';
-import {PerformanceWrapperProps} from '../Form/Helpers/performanceWrapper';
-=======
-import {DropZoneProps, DropZoneFile, PerformanceWrapperProps} from '../../typings/types.d';
->>>>>>> unified-types:src/DropZone/DropZone.jsx
+import {DropZoneProps, PerformanceWrapperProps} from '../../typings/types.d';
 
 /** Styles */
 import './DropZone.scss';
 
 /** Helpers */
-const isFileArray = files => Set.isSet(files) && files.size > 1;
+const isFileArray = (files) => {
+  return Set.isSet(files) && files.size > 1;
+};
 
-const isSingleFile = files => Set.isSet(files) && files.size === 1;
+const isSingleFile = (files) => {
+  return Set.isSet(files) && files.size === 1;
+};
 
-const passPropsToChildren = (props, children) => {
-  if(children.type === 'span' || children.type === 'div') {
+const PassDownProps = (props, children) => {
+  if (children.type === 'span' || children.type === 'div') {
     return React.cloneElement(children);
+  } else {
+    return React.cloneElement(children, {
+      ...props
+    });
   }
-  return React.cloneElement(children, {...props});
-}
+};
 
 /** Class DropZone */
-class DropZone extends React.Component<DropZoneProps & PerformanceWrapperProps, undefined> {
+class DropZoneFrecl extends React.Component<DropZoneProps & PerformanceWrapperProps, undefined> {
+  static defaultProps = {
+    children: <noscript />
+  }
+
   componentDidMount() {
-    this.props.inputChanged(this.getFiles(), false);
-  }
+    const files = this.getFiles();
 
-  getFiles = () => {
-    const {value} = this.props;
-    return value ?  value : Set();
-  }
-<<<<<<< HEAD:src/DropZone/DropZone.tsx
-
-  onDrop = (acceptedFiles: FileList, rejectedFiles: FileList) => {
-    const {multiple = false, inputChanged} = this.props;
-    
-    if(multiple) {
-      inputChanged(Set(this.getFiles().concat(Set(acceptedFiles))));
-    } else {
-      inputChanged(Set([acceptedFiles[0]]));
+    if(files.size > 0) {
+      this.props.inputChanged(this.getFiles(), false);
     }
   }
 
-  deleteFile = (index: number) => {
-=======
+  getFiles = (): Set<any> => {
+    const {value} = this.props;
+
+    if(isFileArray(value)) {
+      return Set(value);
+    } else if(isSingleFile(value)) {
+      return Set([value.first()]);
+    }
+    return Set();
+  }
   
   onDrop = (files) => {
     const {multiple = false, inputChanged, onDrop} = this.props;
     if(multiple) {
-      const allFiles = this.getFiles().concat(files);
+      const allFiles = this.getFiles().concat(files) as Set<File>;
       inputChanged(allFiles);
 
       if(isFunction(onDrop)) {
         onDrop(allFiles);
       }
     } else {
-      inputChanged(Set(files));
+      inputChanged(Set([files.first()]));
       
       if(isFunction(onDrop)) {
-        onDrop(files);
+        onDrop(files.first());
       }
     }
   }
 
   deleteFile = (index) => {
->>>>>>> unified-types:src/DropZone/DropZone.jsx
     const {multiple = false, inputChanged} = this.props;
-    const filesInState = this.getFiles();
-
-    inputChanged(filesInState.delete(index));
+    const stateFiles = this.getFiles();
+    
+    inputChanged(stateFiles.delete(index));
   }
-
+  
   render() {
-    const {children, className, showList = true, fileListComponent} = this.props;
-    
-    const {multiple = false, attributes} = getHTMLAttributes(this.props);
+    const {children, className, placeholder = "Drop here", multiple = false, showList = true, fileListComponent} = this.props;
+    const attributes = getHTMLAttributes(this.props);
+
     const files = this.getFiles();
-    const classes = classnames('drop-zone-box', className);
-    
+    const classes = classnames("drop-zone-box", className);
+
     return (
       <div>
-<<<<<<< HEAD:src/DropZone/DropZone.tsx
-        {/* Drop zone */}
-        {!multiple && files.size > 0 ? <noscript /> : (
-          <div className="drop-zone">
-            <ReactDropzone className={classes} onDrop={this.onDrop} multiple={multiple} {...attributes}>
-              {isFunction(children) ? (
-                children(files, this.deleteFile)
-              ) : (
-                <div>
-                  {children && passPropsToChildren({files}, children)}
-                </div>
-              )}
-            </ReactDropzone>
-          </div>
-        )}
-=======
         <div className="drop-zone">
           <DropZone className={classes} onDrop={this.onDrop} multiple={multiple} {...attributes}>
             {isFunction(children) ? (
@@ -118,20 +103,14 @@ class DropZone extends React.Component<DropZoneProps & PerformanceWrapperProps, 
             )}
           </DropZone>
         </div>
->>>>>>> unified-types:src/DropZone/DropZone.jsx
 
         {isFunction(fileListComponent) && (
-          fileListComponent(files, this.deleteFile)
+          fileListComponent(this.getFiles(), this.deleteFile)
         )}
       </div>
-    );
+    )
   }
 }
 
-<<<<<<< HEAD:src/DropZone/DropZone.tsx
-export default performanceWrapper<DropZoneProps>(DropZone);
-export {DropZone};
-=======
 export default performanceWrapper(DropZoneFrecl);
 export {DropZoneFrecl};
->>>>>>> unified-types:src/DropZone/DropZone.jsx
