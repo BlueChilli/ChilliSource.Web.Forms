919e360d4df074cf4182c9fcea1d87afd7e9cb61
Jeremy Arimado <jeremy@ja.local>
2017-09-06 09:10:08 +1000

    updated formInner content

--- app/FormInner.tsx
+++ app/FormInner.tsx
@@ -39,6 +39,9 @@ export default class FormInner extends React.Component<{}, {}> {
 
     return (
       <div style={{ padding: '20px 40px' }}>
+
+        <h1>Hi</h1>
+
         <Form name="demo-form">
 
           {/* INPUTS */}
