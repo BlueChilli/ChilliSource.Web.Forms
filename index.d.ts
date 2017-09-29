6d4987f95150ad05d454a247737b3dd639379b3c
shane <spp935@gmail.com>
2017-09-29 12:01:14 +1000

    type changes

--- index.d.ts
+++ index.d.ts
@@ -6,97 +6,136 @@
 /// <reference types="react" />
 
 /** Libraries */
-import {Component} from 'react';
-import {Moment} from 'moment';
-import {List, Map} from 'immutable';
+import { Component } from 'react';
+import { Moment } from 'moment';
+import { List, Map } from 'immutable';
 
 /** Components */
-import {WithHandlersGuard, InputTest, ClearAllInputsAction, SwitchProps, OnSubmit,
-        DatePickerProps, DateRangeProps, DropZoneProps, TextInputProps, DateRangeMap,
-        RadioTabProps, RadioTabsProps, SelectInputProps, MultiSelectProps,
-        TextAreaProps, ValidateProps, ValidationComponentProps, FormProps,
-        FieldSetProps, InputGroupProps, InputWrapperProps, ErrorWrapperProps,
-        DisplayValidationProps, PerformanceWrapperProps, FormOwnProps, BaseReactProps} from './typings/types.d';
+import {
+	WithHandlersGuard,
+	InputTest,
+	ClearAllInputsAction,
+	SwitchProps,
+	OnSubmit,
+	DatePickerProps,
+	DateRangeProps,
+	DropZoneProps,
+	TextInputProps,
+	DateRangeMap,
+	RadioTabProps,
+	RadioTabsProps,
+	SelectInputProps,
+	MultiSelectProps,
+	TextAreaProps,
+	ValidateProps,
+	ValidationComponentProps,
+	FormProps,
+	FieldSetProps,
+	InputGroupProps,
+	InputWrapperProps,
+	ErrorWrapperProps,
+	DisplayValidationProps,
+	PerformanceWrapperProps,
+	FormOwnProps,
+	BaseReactProps
+} from './typings/types.d';
 
 export = CSForms;
 export as namespace CSForms;
 
 declare namespace CSForms {
-  
-  // Types and Interfaces
-  // ----------------------------------------------------------------------------
-  type InputUnionProps = TextInputProps | TextAreaProps | SelectInputProps | SwitchProps | RadioTabsProps | DatePickerProps | DateRangeProps
-  
-  type DateRangeMoment = {
-    startDate: Moment;
-    endDate: Moment;
-  }
+	// Types and Interfaces
+	// ----------------------------------------------------------------------------
+	type InputUnionProps =
+		| TextInputProps
+		| TextAreaProps
+		| SelectInputProps
+		| SwitchProps
+		| RadioTabsProps
+		| DatePickerProps
+		| DateRangeProps;
 
-  type PossibleInputValue = number | string | boolean | Moment | DateRangeMap | undefined | List<any>
+	type DateRangeMoment = {
+		startDate: Moment;
+		endDate: Moment;
+	};
 
-  interface FormOptionalProps<T> extends BaseReactProps {
-    /** Accepts different mime types and ensures the user specified onSubmit is called with data in the correct format
+	type PossibleInputValue =
+		| number
+		| string
+		| boolean
+		| Moment
+		| DateRangeMap
+		| undefined
+		| List<any>;
+
+	type EncType = 'application/json' | 'multipart/form-data';
+
+	interface FormOptionalProps<T> extends BaseReactProps {
+		/** Accepts different mime types and ensures the user specified onSubmit is called with data in the correct format
     * currently supports: application/json and multipart/form-data */
-    encType?: 'application/json' | 'multipart/form-data',
-    /** Called before the form is submitted, ths is a chance to modify the contents of the payload
-     * primarily used by the form generator */    
-    mapOutput?: (data?: Map<string, any>) => Map<string, any>,
-    /** Called once Form has ensured that all child Input components are valid */
-    onSubmit?: OnSubmit<T>,
-  }
+		encType?: EncType;
+		/** Called before the form is submitted, ths is a chance to modify the contents of the payload
+     * primarily used by the form generator */
 
-  interface FormOwnProps<T> extends FormOptionalProps<T> {
-    /** Used to namespace all child input components in the redux store or local state */
-    name: string,
-  }
+		mapOutput?: (data?: Map<string, any>) => Map<string, any>;
+		/** Called once Form has ensured that all child Input components are valid */
+		onSubmit?: OnSubmit<T>;
+	}
 
-  // Functions
-  // ----------------------------------------------------------------------------
-  function performanceWrapper<TOutter extends WithHandlersGuard> (Component: React.ComponentType<TOutter & PerformanceWrapperProps>): React.ComponentType<TOutter>
-  
-  function validationsAvailable<T> (inputAttributes:T): InputTest[];
-  
-  function clearAllInputs(nameSpace: string): ClearAllInputsAction;
+	interface FormOwnProps<T> extends FormOptionalProps<T> {
+		/** Used to namespace all child input components in the redux store or local state */
+		name: string;
+	}
 
+	// Functions
+	// ----------------------------------------------------------------------------
+	function performanceWrapper<TOutter extends WithHandlersGuard>(
+		Component: React.ComponentType<TOutter & PerformanceWrapperProps>
+	): React.ComponentType<TOutter>;
 
-  // Components
-  class CheckBox extends Component<SwitchProps, {}>{}
+	function validationsAvailable<T>(inputAttributes: T): InputTest[];
 
-  class DatePicker extends Component<DatePickerProps, {}>{}
+	function clearAllInputs(nameSpace: string): ClearAllInputsAction;
 
-  class DateRange extends Component<DateRangeProps, {}>{}
-  
-  class DropZone extends Component<DropZoneProps, {}>{}
-  
-  class Input extends Component<TextInputProps, {}>{}
+	// Components
+	class CheckBox extends Component<SwitchProps, {}> {}
 
-  class Number extends Component<TextInputProps, {}>{}
+	class DatePicker extends Component<DatePickerProps, {}> {}
 
-  class Radio extends Component<SwitchProps, {}>{}
+	class DateRange extends Component<DateRangeProps, {}> {}
 
-  class RadioTab extends Component<RadioTabProps, {}>{}
+	class DropZone extends Component<DropZoneProps, {}> {}
 
-  class RadioTabs extends Component<RadioTabsProps, {}>{}
+	class Input extends Component<TextInputProps, {}> {}
 
-  class Select extends Component<SelectInputProps, {}>{}
-  
-  class MultiSelect extends Component<MultiSelectProps, {}>{}
+	class Number extends Component<TextInputProps, {}> {}
 
-  class TextArea extends Component<TextAreaProps, {}>{}
+	class Radio extends Component<SwitchProps, {}> {}
 
-  class Validate extends Component<ValidateProps, {}>{}
+	class RadioTab extends Component<RadioTabProps, {}> {}
 
-  class Validation extends Component<ValidationComponentProps, {}>{}
+	class RadioTabs extends Component<RadioTabsProps, {}> {}
 
-  class Form extends Component<FormProps<undefined>, {}>{}
+	class Select extends Component<SelectInputProps, {}> {}
 
-  class Fieldset extends Component<FieldSetProps, {}>{}
+	class MultiSelect extends Component<MultiSelectProps, {}> {}
 
-  class InputGroup extends Component<InputGroupProps, {}>{}
+	class TextArea extends Component<TextAreaProps, {}> {}
 
-  class InputWrapper extends Component<InputWrapperProps, {}>{}
+	class Validate extends Component<ValidateProps, {}> {}
 
-  class ErrorWrapper extends Component<ErrorWrapperProps, {}>{}
-  
-  class DisplayValidation extends Component<DisplayValidationProps, {}>{}
+	class Validation extends Component<ValidationComponentProps, {}> {}
+
+	class Form extends Component<FormProps<undefined>, {}> {}
+
+	class Fieldset extends Component<FieldSetProps, {}> {}
+
+	class InputGroup extends Component<InputGroupProps, {}> {}
+
+	class InputWrapper extends Component<InputWrapperProps, {}> {}
+
+	class ErrorWrapper extends Component<ErrorWrapperProps, {}> {}
+
+	class DisplayValidation extends Component<DisplayValidationProps, {}> {}
 }
